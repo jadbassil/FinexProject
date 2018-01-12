a2a56ef4c6fdf547e7c3238696d9c80da95df22c
jad bassil <jadbassil1@gmail.com>
2018-01-12 19:33:36 +0200

    1/12/2018 19:33

--- app/src/main/java/com/example/jb/project/Expenses.java
+++ app/src/main/java/com/example/jb/project/Expenses.java
@@ -1,11 +1,29 @@
 package com.example.jb.project;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Expenses  extends AppCompatActivity {
     String item1;
@@ -13,7 +31,10 @@ public class Expenses  extends AppCompatActivity {
     ListView list;
     ListView list1;
     TextView expense;
+    ListView expenses;
     TextView txt;
+    EditText cost;
+    Button cancel;
     String []type={"Me","Restaurant","Car","Entertainment","Home","Education","Devices","Gifts","Children","Subscriptions"};
     Integer[] imageId={R.drawable.me,R.drawable.resto,R.drawable.car,R.drawable.entert,R.drawable.home,R.drawable.education,R.drawable.devices,R.drawable.gifts,R.drawable.children,R.drawable.subs};
 
@@ -25,6 +46,10 @@ public class Expenses  extends AppCompatActivity {
         list1 = (ListView) findViewById(R.id.secondaryExp);
         expense = (TextView) findViewById(R.id.expense);
         txt=(TextView)findViewById(R.id.txt);
+        cancel=(Button)findViewById(R.id.buttoncancel);
+        cancel.getBackground().setAlpha(64);
+        expenses=(ListView)findViewById(R.id.expensesList);
+        cost=(EditText)findViewById(R.id.cost);
     }
     public void showlists(View v){
         PrimaryList adapter=new PrimaryList(Expenses.this,type,imageId);
@@ -108,6 +133,79 @@ public class Expenses  extends AppCompatActivity {
                 expense.setText(" "+ item1 +"/"+ item2);
             }
         });
-
+        setListViewHeightBasedOnChildren(list1);
+        setListViewHeightBasedOnChildren(list);
     }
+
+    public  void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+    }
+
+    public void cancel(View v){
+        Intent intent = new Intent(Expenses.this, SpendingActivity.class);
+        startActivity(intent);
+    }
+
+    /*  public void sendExpenseToServer (View v){
+        if(expense.length()==0 || cost.getText().toString().equals("") || cost.getText().toString().equals("0.00")){
+            Toast.makeText(this, "Please add the cost ", Toast.LENGTH_SHORT).show();
+            return ;
+        }
+        final double total = Double.parseDouble(cost.getText().toString());
+
+        class SendPostReq extends AsyncTask<String, Void, String> {
+            SharedPreferences sharedPref = getSharedPreferences("userInfo", MODE_PRIVATE);
+            int id = sharedPref.getInt("user_id", 0);
+
+            @Override
+            protected String doInBackground(String... args){
+                List<NameValuePair> list = new ArrayList<>();
+                list.add(new BasicNameValuePair("total", String.valueOf(total)));
+                list.add(new BasicNameValuePair("id",String.valueOf(id)));
+
+
+                try{
+                    JSONParser j = new JSONParser();
+                    JSONObject obj ;
+                    obj = j.makeHttpRequest("http://10.0.2.2/Finex/expense.php", "POST", list);
+                    return obj.toString();
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+
+            @Override
+            public void onPostExecute(String result){
+                super.onPostExecute(result);
+                try {
+                    JSONObject obj = new JSONObject(result);
+                    Toast.makeText(getApplicationContext(), obj.getString("message").toString(), Toast.LENGTH_SHORT).show();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        SendPostReq spr = new SendPostReq();
+        spr.execute();
+    } */
+
+
 }
